abd75674ef4df93518fa5ac68cdbc5fea839b4b3
Fahrul Riza <rizafahrul16@gmail.com>
2022-08-23 17:00:30 +0700

    api-sapamobile

--- app/Http/Middleware/LihatData.php
+++ app/Http/Middleware/LihatData.php
@@ -17,12 +17,19 @@ class LihatData
      */
     public function handle(Request $request, Closure $next)
     {
-        $access_token = request()->session()->get('access_token');
+       $access_token = request()->session()->get('access_token');
         $response = Http::withToken($access_token)->get('http://sso.politeknikaceh.ac.id/api/user');
         if($response) {
             $request->user = json_decode($response);
+         //   $response 
             return $next($request);
         }
         return response(401)->json("unathorized");
     }
 }
+
+
+// $response = Http::withHeaders([
+            //     'Accept' => 'application/json',
+            //     'Authorization' => 'Bearer '.$access_token,
+            // ]);
